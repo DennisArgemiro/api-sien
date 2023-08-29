3a071dcdd150a81d2e6b3f815d776215dff4fad8
Dennis Argemiro <dennisarg2011@gmail.com>
2023-08-29 17:00:47 -0300

    Omitindo configurações do Email do Serviço

---
+++ config Model.js
@@ -0,0 +1,6 @@
+module.exports = {
+  address: '', // email SMTP
+  passAddress: "", // Senha de aplicativo 
+  host: "", // Servidor do SMTP Ex: Google, Outlook...
+  port: "" // Porta que esta servindo o SMTP
+}
